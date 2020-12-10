ece2b41ec72a5ce1925bb1f19f276d40f06ebfbb
Regina Hansson <regha434@student.liu.se>
2020-12-10 13:33:55 +0100

    Voilà...

--- day10.py
+++ day10.py
@@ -1,4 +1,4 @@
-adapters = [] #list(map(int, input().readlines()))
+adapters = []
 while True:
     try:
         adapters.append(int(input()))
@@ -8,7 +8,7 @@ while True:
 print("A: ")
 
 adapters = sorted(adapters)
-adapters.append(adapters[-1] + 3)
+adapters = [0] + adapters + [adapters[-1] + 3]
 diffs = []
 jolts = 0
 for a in adapters:
@@ -18,36 +18,19 @@ for a in adapters:
 
 print(diffs.count(1) * diffs.count(3))
 
+
 print("B: ")
 
+sums = [0 for _ in range(adapters[-1] + 1)]
 
-adapters = [0] + adapters#[:-1]
-adapters += [adapters[-1] + 3]#[:-1] # Just added this, don't know...
-ways = [0 for _ in range(adapters[-1] + 1)]
-sums = [[] for _ in range(adapters[-1] + 1)]
-ways[0] = 1
-sums[0] = [0]
+for i in adapters:
+    if i == 0:
+        sums[i] = 1
+    elif i == 1:
+        sums[i] = sums[i-1]
+    elif i == 2:
+        sums[i] = sums[i-1] + sums[i-2]
+    else:
+        sums[i] = sums[i-1] + sums[i-2] + sums[i-3]
 
-for i, a in enumerate(adapters):
-    ... #print(i, a)
-
-for i in range(1, len(adapters)):
-    for a in adapters:
-        roof = False
-        for num in sums[i - 1]:
-            roof = a > num + 3
-            if roof or a <= num:
-                continue
-            sums[i].append(a)
-            ways[i] += 1
-       #     if i < 3:
-       #         print(a, sums[i])
-       # if i < 3:
-       #     print()
-        if roof: break
-    if ways[i] < ways[i - 1]:
-        break
-
-print(sums[:3])
-#print(sums, ways)
-print(max(ways))
+print(sums[-1])
