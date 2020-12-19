a3fd0ab1b9836bf6619ba30ff9844efe1dbf0dd0
Regina Hansson <regha434@student.liu.se>
2020-12-19 13:51:00 +0100

    Hurra! :D It took more than 24 hours, but day 18 is beaten.

--- day18.py
+++ day18.py
@@ -35,33 +35,38 @@ def parse_A(row):
     return result
 
 def parse_B(row):
-    #print(f"Parse {row}")
-    char = row.pop(0)
+    new_row = []
 
+    char = row.pop(0)
     if char == "(":
-        result, row = parse_B(row)
+        inner_new, row = parse_B(row)
+        result = parse_A(["("] + inner_new + [")"])
     else:
         result = int(char)
 
     while len(row):
         op = row.pop(0)
         if op == ")":
-            #print(f"{result=}")
-            return result, row
+            new_row.append(result)
+            return new_row, row
 
         char = row.pop(0)
         if char == "(":
-            right, row = parse_B(row)
+            inner_new, row = parse_B(row)
+            right = parse_A(["("] + inner_new + [")"])
         else:
             right = int(char)
 
-
         if op == "*":
-            result *= right
+            new_row.append(result)
+            new_row.append("*")
+            result = right
         if op == "+":
             result += right
-    #print(f"{result=}")
-    return result
+    new_row.append(result)
+
+    a = parse_A(new_row)
+    return a
 
 
 print("A:")
